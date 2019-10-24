fe2131553ba5f54f20d3470f30358a65be4a78fe
1234 <http://github.com/skes6705/1234.git>
2019-10-24 20:34:16 +0800

    test3

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cale cale = new cale();
+            int s1 = cale.Add(3, 5);
+            int s2 = cale.Sub(7, 2);
+            Console.WriteLine("3+5=" + s1);
+            Console.WriteLine("7-2=" + s2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/cale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class cale    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
